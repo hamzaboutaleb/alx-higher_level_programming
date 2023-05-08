3f84b683852a1d4a692969f0cf48e1fcfbdee046
boutalebhamza <boutaleb.hm@gmail.com>
2023-05-08 13:26:11 -0700

    project hello world

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+  * check_cycle - check iflist has cycle
+  * @list: list to check
+  * Return: 0 if no cycle 1 is cycle
+  */
+int check_cycle(listint_t *list)
+{
+	listint_t *fast, *slow;
+
+	fast = list;
+	slow = list;
+	do {
+		if (fast == NULL)
+			return (0);
+		if (slow == fast)
+			return (1);
+		fast = list->next;
+		slow = list->next;
+		if (fast->next)
+			fast = fast->next;
+		else
+			return (0);
+	} while (fast);
+}
