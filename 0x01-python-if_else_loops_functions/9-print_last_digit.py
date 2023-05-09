81b078250dda7f41a31473801838dce45e7bc75a
hamzaboutaleb <hamza-boutaleb@live.fr>
2023-05-09 22:09:28 +0000

    python tasks

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -4,5 +4,5 @@ def print_last_digit(number):
     if number < 0:
         number *= -1
     last_digit = number % 10
-    printf("{}".format(last_digit), end="")
+    print("{}".format(last_digit), end="")
     return last_digit
