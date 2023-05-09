4992bc434d9bed7aac8f8f995c3ce6a3fdf72fb2
hamzaboutaleb <hamza-boutaleb@live.fr>
2023-05-09 21:35:46 +0000

    python tasks

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-for i in range(0, 100):
-    print("{0:0=2d}".format(i), end="")
-    if i != 99:
-        print(", ", end="")
+for i in range(0, 99):
+    print("{0:0=2d}, ".format(i), end="")
+print("{}".format(99))
