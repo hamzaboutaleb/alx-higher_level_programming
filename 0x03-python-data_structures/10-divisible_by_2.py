03aec54c21a1b60fe6ca177041fbb8d1c7e2ab69
boutalebhamza <boutaleb.hm@gmail.com>
2023-05-15 14:00:23 -0700

    ds

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -5,6 +5,6 @@ def divisible_by_2(my_list=[]):
     for el in my_list:
         if el % 2 == 0:
             new_list.append(True)
-        else
+        else:
             new_list.append(False)
     return new_list
