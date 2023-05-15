d9c0caa8d2ee659e89b215c6409db894d91f2a17
hamzaboutaleb <hamza-boutaleb@live.fr>
2023-05-15 21:30:40 +0100

    ds

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,5 @@
 #!/use/bin/pyhton3
 
 def multiple_returns(sentence):
-    new_tuple = (len(sentence), None if len(sentence) == 0 else sentence[0])
+    new_tuple = (len(sentence), None if (len(sentence) == 0) else sentence[0])
     return new_tuple
