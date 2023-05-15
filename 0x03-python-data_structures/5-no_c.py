16f21173ad3dfc551949f01e68ef522786f3c45d
boutalebhamza <boutaleb.hm@gmail.com>
2023-05-15 12:30:27 -0700

    ds

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,6 +3,6 @@
 def no_c(my_string):
     new_str = list(my_string)
     for i in range(len(new_str)):
-        if new_str[i] == "c":
+        if new_str[i] == "c" or new_str[i] == "C":
             del new_str[i]
     return new_str.join("")
