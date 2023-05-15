d43e06ed469e20c86facd7db9f95facadce546b7
boutalebhamza <boutaleb.hm@gmail.com>
2023-05-15 12:59:05 -0700

    ds

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    new_tuple = tuple_a + tuple_b
+    new_tuple = (tuple_a[0] + tuple_b[0], tuple_b[1] + tuple_b[1])
     return new_tuple
