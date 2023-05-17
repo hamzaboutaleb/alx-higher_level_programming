d50fb05d10443fb919d32901c0d6d603a2b858b3
boutalebhamza <boutaleb.hm@gmail.com>
2023-05-17 11:42:41 -0700

    ds

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if a_dictionary is None or len(a_dictionary) == 0:
         return None
-    keys = a_dictionary.keys()
-    max_val = a_dictionary[keys[0]]
+
+    keys = list(a_dictionary.keys())
+    max_val = a_dictionary.get(keys[0])
     key = keys[0]
-    for k in keys:
+    for k in a_dictionary:
         if a_dictionary[k] > max_val:
             max_val = a_dictionary[k]
             key = k
