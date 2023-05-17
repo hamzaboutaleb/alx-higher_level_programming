f3265a90bf4a5ea44f74ba0797b70fd17aa62f55
boutalebhamza <boutaleb.hm@gmail.com>
2023-05-17 10:50:15 -0700

    ds

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def uniq_add(my_list=[]):
+    new_list = []
+    visited = set()
+    for el in my_list:
+        if el in visited:
+            continue
+        new_list.append(el)
+        visited.add(el)
+    new_list
