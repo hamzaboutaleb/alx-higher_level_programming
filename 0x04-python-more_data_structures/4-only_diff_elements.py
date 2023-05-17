e2b762729f4c2986c835438546b1a83a1db54414
boutalebhamza <boutaleb.hm@gmail.com>
2023-05-17 11:10:20 -0700

    ds

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 
 def only_diff_elements(set_1, set_2):
-    new_set = set()
-    new_set.add(set_1.difference(set_2))
-    new_set.add(set_2.difference(set_2))
-    return new_set
+    return set_1 ^ set_2
