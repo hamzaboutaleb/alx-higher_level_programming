b1fb1148526beefebacb877b09bbcf957c98832b
boutalebhamza <boutaleb.hm@gmail.com>
2023-05-29 13:16:12 -0700

    python

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,11 +3,10 @@
 
 def safe_print_list(my_list=[], x=0):
     index = 0
-    while index <= x:
+    while index < x:
         try:
             print(my_list[index], end="")
             index++
         except IndexError:
-            print("out of range")
             return index
     return index
