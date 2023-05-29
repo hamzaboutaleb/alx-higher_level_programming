70bd1aa7e710fe248073a85e55142b8d11fb6013
boutalebhamza <boutaleb.hm@gmail.com>
2023-05-29 13:14:05 -0700

    task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def safe_print_list(my_list=[], x=0):
+    index = 0
+    while index < x:
+        try:
+            print(my_list[index], end="")
+            index++
+        except IndexError:
+            print("out of range")
+            return index
+    return index
