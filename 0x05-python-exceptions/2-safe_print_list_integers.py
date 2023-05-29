a1ccdc8e4fedd51263e2d8cfd8fb0d61111a1e6a
boutalebhamza <boutaleb.hm@gmail.com>
2023-05-29 13:45:28 -0700

    python

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    i = count_printed = 0
+    while i < x:
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            i += 1
+            count_printed += 1
+        except (ValueError, TypeError):
+            i += 1
+    print()
+    return count_printed
