73941af9d5549ee52e84c84ba6e8c0c05921e8bd
boutalebhamza <boutaleb.hm@gmail.com>
2023-06-01 11:07:32 -0700

    python classes

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -20,7 +20,7 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
 
-    def area():
+    def area(self):
         """Area of square
 
             Returns:
