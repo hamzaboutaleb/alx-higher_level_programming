3e33bb56a89cbd1d5b1c8edd0c1eb8213b2cb311
boutalebhamza <boutaleb.hm@gmail.com>
2023-06-01 11:09:34 -0700

    python classes

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -9,21 +9,24 @@ class Square:
         """Constructor
 
             Args:
-                size: size of Sqaure
+                size: size of Sqaure.
+
             Raises:
-                TypeError: size is not integer
-                ValueError: if size less than 0
+                TypeError: size is not integer.
+                ValueError: if size less than 0.
         """
         if not isinstance(suze, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
+
         self.__size = size
 
     def area(self):
         """Area of square
 
             Returns:
-                size of square
+                size of square.
+
         """
         return self.__size ** 2
