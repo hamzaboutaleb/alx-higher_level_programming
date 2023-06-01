882538bf018258f0c0d72b4d5b6b8955e2ac931f
boutalebhamza <boutaleb.hm@gmail.com>
2023-06-01 10:50:04 -0700

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+""" Square module"""
+
+
+class Square:
+    """square class"""
+    def __init__(self, size):
+        """
+        Args:
+            size: size of square
+        """
+    self.__size = size
