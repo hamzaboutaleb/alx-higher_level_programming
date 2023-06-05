9930a1e7a19e044fd7c7659139dd061f92967ce0
boutalebhamza <boutaleb.hm@gmail.com>
2023-06-05 09:57:22 -0700

    python oop

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,7 +6,9 @@ Rectangle class module
 
 class Rectangle:
     """ Rectangle class"""
-    
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
     @property
     def width(self):
         return self.__width
