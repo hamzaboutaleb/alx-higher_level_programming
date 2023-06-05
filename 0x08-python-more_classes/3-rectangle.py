f315411bf5a144c42b55ba9a72f3c3e98cbcb288
boutalebhamza <boutaleb.hm@gmail.com>
2023-06-05 10:51:42 -0700

    python oop

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -50,7 +50,6 @@ class Rectangle:
     def __str__(self):
         if self.width == 0 or self.height == 0:
             return ""
-        
         rect = ""
 
         for i in range(self.height):
