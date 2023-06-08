14737ef5f47c53f99dcac3f06be42eaafd6920bf
boutalebhamza <boutaleb.hm@gmail.com>
2023-06-08 13:15:20 -0700

    tdd python

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -13,23 +13,27 @@ def matrix_divided(matrix, div):
      """
 
     if not isinstance(matrix, list):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError("matrix must be a matrix \
+                (list of lists) of integers/floats")
     if len(matrix) == 0:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError("matrix must be a matrix \
+                (list of lists) of integers/floats")
 
     for i in range(1, len(matrix)):
         if len(matrix[i]) != len(matrix[0]):
-            raise TypeError("Each row of the matrix must have the same size")
+            raise TypeError("Each row of the matrix \
+                    must have the same size")
 
     for i in range(len(matrix)):
         for el in matrix[i]:
             if not isinstance(el, int) and not isinstance(el, float):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError("matrix must be a matrix \
+                        list of lists) of integers/floats")
     if not isinstance(div, int) and not isinstance(div, float):
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    
+
     new_matrix = [x[:] for x in matrix]
     for row in new_matrix:
         for i in range(len(row)):
