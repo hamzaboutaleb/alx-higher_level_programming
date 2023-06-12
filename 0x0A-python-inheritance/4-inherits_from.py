2cc9cd855b918f70e5c44f4a97ca2c3825c15566
boutalebhamza <boutaleb.hm@gmail.com>
2023-06-12 14:44:34 -0700

    task 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -3,12 +3,13 @@
 
 
 def inherits_from(obj, a_class):
-    """check if object instance of class thet inherited (directly or indirectly)
+    """check if object instance of class thet \
+            inherited (directly or indirectly)
         Args:
             obj: object
             a_class: class
     """
-    
+
     if type(obj) != a_class and isinstance(obj, a_class):
         return True
     return False
