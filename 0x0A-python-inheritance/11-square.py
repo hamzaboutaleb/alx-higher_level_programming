b95a697b092108aab601966629d37f288c890f4b
boutalebhamza <boutaleb.hm@gmail.com>
2023-06-12 15:36:00 -0700

    task 11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""square module"""
+
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+"""squre class"""
+
+
+class Square(Rectangle):
+    """Square class"""
+
+    def __init__(self, size):
+        """init squre"""
+        self.__size = size
+        super().__init__(self.__size, self.__size)
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return "[Square {}/{}]".format(self.__size, self.__size)
