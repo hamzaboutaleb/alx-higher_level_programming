9ec46da8df97231915dd8060ad6e0b56014680a7
boutalebhamza <boutaleb.hm@gmail.com>
2023-06-12 14:30:53 -0700

    task 2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""is same class module"""
+
+
+def is_same_class(obj, a_class):
+    """check if obj instance of class
+        Args:
+            obj: object
+            a_class: class
+    """
+
+    if isinstance(obj, a_class):
+        return True
+    return False
