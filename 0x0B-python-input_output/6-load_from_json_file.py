7adfa12617a0430e6b4724066cb73a1e9c22389b
boutalebhamza <boutaleb.hm@gmail.com>
2023-06-13 16:00:43 -0700

    write

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" laod object from json"""
+
+import json
+
+
+def load_from_json_file(filename):
+    """create obj from json file
+        Args:
+            filename: filename
+        """
+
+    with open(filename, "r", encoding="utf-8") as f:
+        return josn.loads(f.read())
