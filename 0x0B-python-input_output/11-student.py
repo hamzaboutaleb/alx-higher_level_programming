6cbe96e22183dbe9da3e1b8c7989fda593fda0b7
boutalebhamza <boutaleb.hm@gmail.com>
2023-06-13 16:22:51 -0700

    write

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""student class"""
+
+
+class Student:
+    """student class"""
+
+    def __init__(self, f_n, l_n, age):
+
+        self.first_name = f_n
+        self.last_name = l_n
+        self.age = age
+
+    def to_json(self, attrs=None):
+        
+        if type(attrs) == list:
+            for attr in attrs:
+                if type(attr) != str:
+                    return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
+    def reload_from_json(self, json):
+        """ from json to stuedent class 
+            Args:
+                json: json dict
+        """
+        for key, value in json.items():
+            seattr(self, key, value)
