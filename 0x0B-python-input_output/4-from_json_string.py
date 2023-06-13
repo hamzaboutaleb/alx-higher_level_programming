a13d9e0bef195ea37e8cadedc8f29f2a380de1ff
boutalebhamza <boutaleb.hm@gmail.com>
2023-06-13 15:45:06 -0700

    write

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/pyhton3
+""" from json to obj"""
+
+import json
+
+
+def from_json_string(my_str):
+    """from json to string
+        Args:
+            my_str: string to convert
+    """
+    return json.loads(my_str)
