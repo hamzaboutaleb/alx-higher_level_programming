84f1f8049c5bc6000574bb7a1c1bcf76d990fc43
boutalebhamza <boutaleb.hm@gmail.com>
2023-06-13 15:06:30 -0700

    append write

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -8,4 +8,4 @@ def append_write(filename="", text=""):
             text: text to append
      """
     with open(filename, "w", encoding="utf-8") as f:
-        f.write(text)
+        return f.write(text)
