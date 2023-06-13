d02a00e7ada6ced05751876f8720df300d3a1fb8
boutalebhamza <boutaleb.hm@gmail.com>
2023-06-13 16:16:21 -0700

    write

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""student class"""
+
+
+class Student:
+    """student class"""
+
+    def __init__(self, f_n, l_n, age):
+
+        self.first_name = f_n
+        self.last_name = l_n
+        self.age = age
+
+    def to_json():
+
+        return self.__dict__
