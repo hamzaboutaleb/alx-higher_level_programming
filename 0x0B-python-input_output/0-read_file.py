40472115b2bb0376d36ed6da6f785f91326405cc
boutalebhamza <boutaleb.hm@gmail.com>
2023-06-13 14:59:55 -0700

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,5 +8,5 @@ def read_file(filename=''):
         Args:
             filename: ""filename
     """
-    with open(filename, encoding="utf-8) as file:
-        print(file.read())
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read(), end="")
