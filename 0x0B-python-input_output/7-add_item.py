cd31dad527d470ed35edbe5d1b2c2f6a30596bef
boutalebhamza <boutaleb.hm@gmail.com>
2023-06-13 16:11:55 -0700

    write

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""add item"""
+"""add item to file"""
 
 import sys
 
@@ -8,5 +8,7 @@ if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
     load_from_json = __import__('6-load_from_json_file').load_from_json_file
     items = load_from_json("add_item.json")
+    if not items:
+        items = []
     items.extend(sys.argv[1:])
     save_to_json_file(items, "add_item.json")
