bb3cb0139c5139130f94d586009a3ff8031d8657
boutalebhamza <boutaleb.hm@gmail.com>
2023-06-13 15:42:01 -0700

    write

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" obj to json"""
+
+import json
+
+
+def to_json_string(my_obj):
+    """ convert object to json
+        Args:
+            my_obj: object
+     """
+     return json.dumps(my_obj)
