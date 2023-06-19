f947eb509fe108c4dc19698edddb3a19cc69cd67
hamzaboutaleb <hamza-boutaleb@live.fr>
2023-06-19 20:11:23 +0000

    test

---
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -0,0 +1,20 @@
+#/usr/bin/python3
+"""base class Test module"""
+
+import unittest
+from models.base import Base
+
+
+class TestBaseClass(unittest.TestCase):
+
+    def test_without_id(self):
+        base = Base()
+        self.assertEqual(base.id, 1)
+    
+    def test_with_id(self):
+        b = Base(15)
+        self.assertEqual(base.id, 15)
+
+
+if __name__ == "__main__":
+    unittest.main()
