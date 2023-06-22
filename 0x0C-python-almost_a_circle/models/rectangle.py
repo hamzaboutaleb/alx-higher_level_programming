ca8d37962b2d63c30f5d7efea08ac373a93bb518
hamzaboutaleb <hamza-boutaleb@live.fr>
2023-06-22 08:00:42 +0000

    rect style

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -83,7 +83,7 @@ class Rectangle(Base):
 
     def __str__(self):
         return "[Rectangle] ({}) {}/{} - {}/{}"
-        .format(self.id, self.x, self.y, self.width, self.height)
+                .format(self.id, self.x, self.y, self.width, self.height)
 
     def display(self):
         """ display rectangle width height and x y """
