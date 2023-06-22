78a2dd69149b357892f825cf791811320ed81a58
hamzaboutaleb <hamza-boutaleb@live.fr>
2023-06-22 07:53:41 +0000

    fix tests

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -15,6 +15,14 @@ class TestBaseClass(unittest.TestCase):
         b = Base(15)
         self.assertEqual(b.id, 15)
 
+    def test_pep8_model(self):
+            """
+            Tests for pep8 model
+            """
+        p8 = pep8.StyleGuide(quiet=True)
+        p = p8.check_files(['models/base.py'])
+        self.assertEqual(p.total_errors, 0, "fix pep8")
+
 
 if __name__ == "__main__":
     unittest.main()
--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -2,6 +2,7 @@
 """base class Test module"""
 
 import unittest
+import pep8
 from models.base import Base
 
 
@@ -14,6 +15,13 @@ class TestBaseClass(unittest.TestCase):
     def test_with_id(self):
         b = Base(15)
         self.assertEqual(b.id, 15)
+
+    def test_pep8_model(self):
+        """Tests for pep8 model"""
+
+        p8 = pep8.StyleGuide(quiet=True)
+        p = p8.check_files(['models/base.py'])
+        self.assertEqual(p.total_errors, 0, "fix pep8")
 
 
 if __name__ == "__main__":
