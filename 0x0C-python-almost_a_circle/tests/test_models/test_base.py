31eba597edf0659680f25ea170d4dc51abb88778
hamzaboutaleb <hamza-boutaleb@live.fr>
2023-06-22 08:21:16 +0000

    tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -9,20 +9,16 @@ from models.base import Base
 class TestBaseClass(unittest.TestCase):
 
     def test_without_id(self):
+        """ test without setting id"""
         base = Base()
+        b = Base()
         self.assertEqual(base.id, 1)
+        self.assertEqual(b.id, 2)
     
     def test_with_id(self):
+        """ test setting id """
         b = Base(15)
         self.assertEqual(b.id, 15)
-
-    def test_pep8_model(self):
-        """Tests for pep8 model"""
-
-        p8 = pep8.StyleGuide(quiet=True)
-        p = p8.check_files(['models/base.py'])
-        self.assertEqual(p.total_errors, 0, "fix pep8")
-
 
 if __name__ == "__main__":
     unittest.main()
