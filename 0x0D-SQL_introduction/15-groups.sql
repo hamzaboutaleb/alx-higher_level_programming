d0cc55b55a775d8d9853408e6da97bd905fbc3b3
boutalebhamza <boutaleb.hm@gmail.com>
2023-07-18 13:53:38 -0700

    15

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,3 +1,3 @@
 --  lists the number of records with the same score in the table second_table
 
-SELECT score, COUNT(*) as number FROM second_table ORDER BY number DESC GROUP BY score;
+SELECT score, COUNT(*) as number FROM second_table GROUP BY score ORDER BY number DESC;
