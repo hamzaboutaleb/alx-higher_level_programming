a2fd36a9fdb27b912004715d1ba9313915a5a745
boutalebhamza <boutaleb.hm@gmail.com>
2023-07-19 17:25:56 -0700

    task 16

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- lists all Comedy shows in the database hbtn_0d_tvshows.
 
-SELECT t.title FROM tv_shows t
+SELECT t.title, g.name FROM tv_shows t
 LEFT JOIN tv_show_genres s ON t.id = s.show_id
 JOIN tv_genres g ON g.id = s.genre_id
 ORDER BY t.title, g.name;
