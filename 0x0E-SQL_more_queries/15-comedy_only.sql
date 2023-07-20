aa8afd336fd590f8afbd6ce778f4362e9479d228
boutalebhamza <boutaleb.hm@gmail.com>
2023-07-19 17:16:25 -0700

    task 15

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,7 @@
+-- lists all Comedy shows in the database hbtn_0d_tvshows.
+
+SELECT t.title FROM tv_shows t
+JOIN tv_show_genres s ON t.id = s.show_id
+JOIN tv_genres g ON g.id = s.genre_id
+WHERE g.name = "Comedy"
+ORDER BY t.title ASC;
