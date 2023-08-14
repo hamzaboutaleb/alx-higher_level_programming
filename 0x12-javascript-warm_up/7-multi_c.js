666da3ff05cff5be4bbdb8ec72b32312f22330f3
hamzaboutaleb <hamza-boutaleb@live.fr>
2023-08-14 20:53:28 +0100

    task 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
-
 let num = process.argv[2];
-
-if (!isNaN(parseInt(process.argv[2]))) {
+if (!isNaN(parseInt(num))) {
   for (let i = 0; i < num; i++) {
     console.log('C is fun');
   }
