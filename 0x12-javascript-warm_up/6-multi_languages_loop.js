6dca35b0d88cfc3686ded8dbb529e62bb2c8b753
hamzaboutaleb <hamza-boutaleb@live.fr>
2023-08-14 20:35:36 +0100

    task 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const lang = ['C is fun', 'Python is coo', 'JavaScript is amazing'];
+let lang = ['C is fun', 'Python is coo', 'JavaScript is amazing'];
 
 for (let i = 0; i < lang.length; i++) {
   console.log(lang[i]);
