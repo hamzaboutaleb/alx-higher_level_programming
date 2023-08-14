039b0d0d52db10beb8cb4e17e86f0445faf6ad81
hamzaboutaleb <hamza-boutaleb@live.fr>
2023-08-14 19:56:05 +0100

    task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const argv = process.argv
-if (argv.length - 2 == 0) {
+if (argv.length - 2 === 0) {
   console.log('No argument');
-} else if (argv.length - 2 == 1) {
+} else if (argv.length - 2 === 1) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
