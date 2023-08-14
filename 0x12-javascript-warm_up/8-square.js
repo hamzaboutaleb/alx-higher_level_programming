26f4b35fdd487ff01d1723a4df3ac80dc87ef528
hamzaboutaleb <hamza-boutaleb@live.fr>
2023-08-14 21:37:08 +0100

    task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,11 +3,11 @@ const size = process.argv[2];
 let x = "";
 if (isNaN(parseInt(size))) {
   console.log('Missing size');
-} else {
-  for(let i = 0; i < size; i++) {
-    x += "X";
-  }
-  for (let j = 0; j < size; j++) {
-    console.log(x);
-  }
+  process.exit(0);
+} 
+for(let i = 0; i < size; i++) {
+  x += "X";
+}
+for (let j = 0; j < size; j++) {
+  console.log(x);
 }
