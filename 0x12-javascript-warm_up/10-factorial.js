5753b791f16a6986e2fa3a85fbeeb5f53d00f39d
hamzaboutaleb <hamza-boutaleb@live.fr>
2023-08-14 21:58:08 +0100

    task 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-const n = process.argv[2];
+const n = parseInt(process.argv[2]);
 
 console.log(fact(n));
 function fact(n) {
   if (isNaN(n)) return 1;
-  if (n == 0) return 1;
+  if (n === 1) return 1;
 
-  return n * fact(n-1);
+  return (n * fact(n-1));
 }
