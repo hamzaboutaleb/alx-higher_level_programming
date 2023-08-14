74bd8c175bc4492f1e682dca015998d0d0d3c727
hamzaboutaleb <hamza-boutaleb@live.fr>
2023-08-14 22:05:04 +0100

    task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,13 +1,18 @@
 #!/usr/bin/node
 const argv = process.argv;
-let max = 0;
+let max = 0, max2 = 2;
 if (argv.length <= 3) {
   console.log(0);
 } else {
-  max = argv[2];
+  max = 2;
+
   for (let i = 3; i < argv.length; i++) {
     let num = parseInt(argv[i]);
-    if (num > max) max = num;
+    if (num > argv[max]) max = i;
   }
-  console.log(max);
+  for (let i = 2; i < argv.length; i++) {
+    let num = parseInt(argv[i]);
+    if (num > argv[max2] && i != max) max2 = i;
+  }
+  console.log(argv[max2]);
 }
