44eef75361f728ed4ced41885335bb7099d24ca7
hamzaboutaleb <hamza-boutaleb@live.fr>
2023-08-14 20:25:19 +0100

    task 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-const isNumber = (num) => !isNaN(parseInt(num));
-if (!isNumber(process.argv[2])) {
+if (isNaN(parseInt(process.argv[2]))) {
   console.log('Not a number')
 } else {
   console.log('My number: ' + parseInt(process.argv[2]));
