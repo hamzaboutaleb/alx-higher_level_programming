9915eb382fb72d8c120c19f1e78831e42b032cb2
hamzaboutaleb <hamza-boutaleb@live.fr>
2023-08-16 00:07:08 +0100

    task 102

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
-
 const fs = require('fs');
 
 const fileA = process.argv[2];
 const fileB = process.argv[3];
 const fileC = process.argv[4];
 
-const dataA =  fs.readFileSync('./' + fileA, 'utf8');
-const dataB =  fs.readFileSync('./' + fileB, 'utf8');
+const dataA = fs.readFileSync('./' + fileA);
+const dataB = fs.readFileSync('./' + fileB);
 fs.writeFileSync('./' + fileC, dataA + dataB);
