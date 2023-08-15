61c92aab5667b6e6a84c1da35d119d7608ece6c1
hamzaboutaleb <hamza-boutaleb@live.fr>
2023-08-15 22:05:46 +0100

    task 3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,20 +1,22 @@
 #!/usr/bin/node
 
-module.exports = class Rectangle {
+class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
   }
-  
+
   print () {
     let x = '';
     for (let i = 0; i < this.width; i++) {
       x += 'X';
     }
-    for (let i = 0; i < this.height; i++) {
+    for (let j = 0; j < this.height; j++) {
       console.log(x);
     }
   }
 };
+
+module.exports = Rectangle;
