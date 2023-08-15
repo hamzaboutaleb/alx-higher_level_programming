4bc93cf06d3fb8a40b0313abcbfa20366faea5b7
hamzaboutaleb <hamza-boutaleb@live.fr>
2023-08-15 23:43:40 +0100

    task 101

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,10 @@
+#!/sur/bin/node
+
+const dict = require('./101-data').dict;
+const newDict = {};
+
+for (const [key, value] of dict.entries()) {
+  newDict[value] = newDict[value]?.push(key) || [];
+}
+
+console.log(newDict);
