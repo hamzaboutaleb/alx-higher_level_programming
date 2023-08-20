7dfc6149dd94168d23a4449f8d7f88257bd74ace
boutalebhamza <boutaleb.hm@gmail.com>
2023-08-20 03:07:45 -0700

    task 7

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+script that lists all State objects from the database hbtn_0e_6_usa
+"""
+
+from sys import argv
+from model_state import State, Base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+if __name__ == "__main__":
+    usr, pwd, db = argv[1:4]
+    con = "mysql+mysqldb://{}:{}@localhost/{}".format(usr, pwd, db)
+    engine = create_engine(con, pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).order_by(State.id).all()
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
+    session.close()
