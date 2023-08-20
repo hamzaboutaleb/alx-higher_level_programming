de7b0a928bb63cce2e2d4cff7c65daeef1a6988a
boutalebhamza <boutaleb.hm@gmail.com>
2023-08-20 04:05:42 -0700

    task 12

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+changes the name of a State object from the database hbtn_0e_6_usa
+"""
+
+from sys import argv
+from model_state import State, Base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+if __name__ == "__main__":
+    usr, pwd, db = argv[1:4]
+    con = "mysql+mysqldb://{}:{}@localhost/{}".format(usr, pwd, db)
+    engine = create_engine(con, pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.get(State, 2)
+
+    if state:
+        state.name = "New Mexico"
+        session.commit()
+    session.close()
