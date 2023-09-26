b21b0385c76f55bd3d5b9e4eaff492d41099c761
hamzaboutaleb <hamza-boutaleb@live.fr>
2023-09-26 13:26:48 -0700

    ttask 5

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const request = require('request');
+
+const args = process.argv;
+const url = args[2];
+const filePath = args[3];
+
+request(url, (err, resp, body) => {
+  if (err) return console.log(err);
+  if (resp.statusCode !== 200) console.log('Request failed');
+
+  writeTo(filePath, body);
+});
+
+function writeTo (filePath, content) {
+  fs.writeFile(filePath, content, 'utf-8', err => console.log(err));
+}
