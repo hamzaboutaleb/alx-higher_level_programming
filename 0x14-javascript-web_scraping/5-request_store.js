953843f7ab0e9c48e9be8bfcb82953273413bfd9
hamzaboutaleb <hamza-boutaleb@live.fr>
2023-09-26 13:29:18 -0700

    task 5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -9,11 +9,14 @@ const filePath = args[3];
 
 request(url, (err, resp, body) => {
   if (err) return console.log(err);
-  if (resp.statusCode !== 200) console.log('Request failed');
+  if (resp.statusCode !== 200) return console.log('Request failed');
 
   writeTo(filePath, body);
+
 });
 
 function writeTo (filePath, content) {
-  fs.writeFile(filePath, content, 'utf-8', err => console.log(err));
+  fs.writeFile(filePath, content, 'utf-8', err => {
+    if (err) return console.log(err);
+  });
 }
