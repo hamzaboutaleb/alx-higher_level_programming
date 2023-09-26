31075a5061697119c5d663dff2c9737414cbcd2e
hamzaboutaleb <hamza-boutaleb@live.fr>
2023-09-26 12:48:01 -0700

    task 2

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const args = process.argv;
+
+const url = args[2];
+
+request(url, (err, response) => {
+  if (err) console.log(err);
+  console.log(`code: ${response.statusCode}`);
+});
